e64613d73ad971d07ad3c857546c51de62747a53
joebakid <josephbawo@gmail.com>
2025-01-23 15:35:21 +0300

    vercel.json

--- src/components/FemaleBags.jsx
+++ src/components/FemaleBags.jsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import WatchGrid from "./WatchGrid";
 import bags from "../femalebags.json"; // Import bags JSON data
 
 function FemaleBags({ Btn }) {
+  useEffect(() => {
+    document.title = "Female Bags"; // Update the tab name
+  }, []); // Empty dependency array means this effect runs once on mount
+
   return <WatchGrid Btn={Btn} items={bags} />;
 }
 
--- src/components/watches.jsx
+++ src/components/watches.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import WatchGrid from "./WatchGrid";
 import watches from "../watches.json"; // Import watches JSON data
 
 function Watches({ Btn }) {
+  useEffect(() => {
+    document.title = "Watch"; // Update the tab name
+  }, []); // Empty dependency array means this effect runs once on mount
   return <WatchGrid Btn={Btn} items={watches} />;
 }
 
