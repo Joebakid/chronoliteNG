42a816adff5d9bc4d4bdd4949ae259ce7d10b778
joebakid <josephbawo@gmail.com>
2025-02-04 02:21:43 +0300

    updated meta data

--- src/components/FemaleBags.jsx
+++ src/components/FemaleBags.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import { Helmet } from "react-helmet-async";
 import WatchGrid from "./WatchGrid";
-import bags from "../femalebags.json"; // Import bags JSON data
+import bags from "../femalebags.json"; // Importing JSON data
 
 function FemaleBags({ Btn }) {
   useEffect(() => {
-    document.title = "Female Bags"; // Update the tab name
+    document.title = "Female Bags - Chronolite NG"; // Updates browser tab title
   }, []);
 
   return (
@@ -19,7 +19,7 @@ function FemaleBags({ Btn }) {
         />
         <meta
           property="og:image"
-          content="https://chronolite.com.ng/path-to-bag-image.jpg"
+          content="https://chronolite.com.ng/path-to-female-bags-image.jpg"
         />
         <meta
           property="og:url"
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async"; // Import HelmetProvider
 import App from "./App";
 import { Btn } from "./components/Btn"; // Reusable button component
 import "./index.css"; // Global styles
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* Pass Btn component to App as a prop for reusability */}
-    <App Btn={Btn} />
+    <HelmetProvider>
+      {/* Pass Btn component to App as a prop for reusability */}
+      <App Btn={Btn} />
+    </HelmetProvider>
   </React.StrictMode>
 );
