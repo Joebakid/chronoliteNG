3b3828176dfa3b95790bfb66a73f344fa1a18d94
joebakid <josephbawo@gmail.com>
2025-03-17 15:02:43 +0100

    whatsap socials

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -33,13 +33,14 @@ function Footer() {
             TikTok: @chronoliteng
           </a>
           <a
-  href="https://wa.me/09037291405"
+  href="https://wa.me/2349037291405"
   target="_blank"
   rel="noopener noreferrer"
   className="text-blue-400 hover:underline"
 >
   WhatsApp: 09037291405
 </a>
+
         </div>
       </div>
     </footer>
