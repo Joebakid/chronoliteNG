becc00074b97e820b680baa0bcaffb0a5b99713a
joseph bawo <josephbawo@gmail.com>
2025-11-11 23:17:06 +0100

    added new item

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { HelmetProvider } from "react-helmet-async"; // Import HelmetProvider
 import App from "./App";
 import { Btn } from "./components/Btn"; // Reusable button component
 import "./index.css"; // Global styles
-
+ 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
